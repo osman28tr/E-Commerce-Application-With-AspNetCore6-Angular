461bff9daeb891c6dabdd868a81ef7ee98506d2c
osman28tr <osmantonbul2001@gmail.com>
2022-09-23 23:15:54 +0300

    Add project files.

---
+++ ETicaretMvc/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretMvc.Application.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult GetProducts()
+        {
+            var products = _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
---
+++ Infrastucture/ETicaretMvc.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretMvc.Application.Abstractions;
+using ETicaretMvc.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretMvc.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
