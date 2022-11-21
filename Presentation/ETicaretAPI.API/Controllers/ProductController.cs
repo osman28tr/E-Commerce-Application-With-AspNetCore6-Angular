614f8e8e57abaab1849dfb4f824858cbad497693
osman28tr <osmantonbul2001@gmail.com>
2022-11-21 12:15:52 +0300

    api eklendi....

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using ETicaretMvc.Application.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private IProductReadRepository productReadRepository;
+        private IProductWriteRepository productWriteRepository;
+        private IOrderReadRepository orderReadRepository;
+        private IOrderWriteRepository orderWriteRepository;
+        private ICustomerWriteRepository customerWriteRepository;
+        public ProductController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository, IOrderReadRepository orderReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository)
+        {
+            this.productReadRepository = productReadRepository;
+            this.productWriteRepository = productWriteRepository;
+            this.orderReadRepository = orderReadRepository;
+            this.orderWriteRepository = orderWriteRepository;
+            this.customerWriteRepository = customerWriteRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
