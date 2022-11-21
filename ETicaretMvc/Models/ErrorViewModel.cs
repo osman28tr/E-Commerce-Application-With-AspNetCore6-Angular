703f33533ae3d1e89bd4b8af5fd5bddeca0e2f76
osman28tr <osmantonbul2001@gmail.com>
2022-11-21 12:15:39 +0300

    api eklendi

--- ETicaretMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ETicaretMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
